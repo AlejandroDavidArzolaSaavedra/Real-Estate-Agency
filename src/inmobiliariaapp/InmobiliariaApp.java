4b9c36b1ff5fdd1ac2a7f9b4da3f3810870151dc
juancad <juancarlosalcd@gmail.com>
2021-12-30 01:38:04 +0100

    creación AgenciaForm, formulario de inicio de la aplicación

--- src/inmobiliariaapp/InmobiliariaApp.java
+++ src/inmobiliariaapp/InmobiliariaApp.java
@@ -1,18 +1,14 @@
-
 package inmobiliariaapp;
 
-import model.Cliente;
-import model.Inmueble;
-import static model.TipologiaInmueble.CHALET;
-
+import gui.AgenciaForm;
 
 public class InmobiliariaApp {
 
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("asd","asd","asd","ads");
-        System.out.println(cliente1);
-        //Inmueble inmu = new Inmueble(CHALET,20.2,5,"Bilbao");
         
+        System.err.println("Hola");
+        AgenciaForm agenciaForm=new AgenciaForm();
+        agenciaForm.setVisible(true);
     }
     
 }
