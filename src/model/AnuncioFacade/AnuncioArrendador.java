f6518a48234f1a26619c81326f9b3a073c9e2475
Alejandro  David Arzola Saavedra <90756437+AlejandroDavidArzolaSaavedra@users.noreply.github.com>
2022-01-07 12:44:39 +0000

    Delete src directory

--- src/model/AnuncioFacade/AnuncioArrendador.java
+++
@@ -1,9 +0,0 @@
-
-package model.AnuncioFacade;
-
-public class AnuncioArrendador {
-    public void arrendarPiso(){}
-    public void mirarArrendamientos(){}
-    public void mirar(){
-        System.out.println("Piso interesado por el arrendador");}
-}
--- src/model/AnuncioFacade/AnuncioVendedor.java
+++
@@ -1,7 +0,0 @@
-
-package model.AnuncioFacade;
-
-public class AnuncioVendedor {
-    public void mirar(){
-        System.out.println("Piso vendido");}
-}
